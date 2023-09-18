a9694dac3aa956768e7c3aa323b6b26a457f5f9b
bristinborah <bristin.borah.7@gmail.com>
2023-09-18 17:13:34 +0530

    yui

--- src/dbconfig/dbconfig.ts
+++ src/dbconfig/dbconfig.ts
@@ -1,22 +1,24 @@
-import mongoose from "mongoose";
-require('dotenv').config({ path: 'MONGO_URI' });
-const dotenv = require("dotenv");
+import mongoose from 'mongoose';
 
-dotenv.config();
-
-export async function connect(){
+export async function connect() {
     try {
         mongoose.connect(process.env.MONGO_URI!);
         const connection = mongoose.connection;
-        connection.on('connected', ()=>{
-            console.log("Mongo DB connected successfully");
+
+        connection.on('connected', () => {
+            console.log('MongoDB connected successfully');
         })
-        connection.on('error', (err)=>{
-            console.log("MongoDB connection error" +err)
+
+        connection.on('error', (err) => {
+            console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err);
             process.exit();
         })
+
     } catch (error) {
-        console.log('Something went wrong');
+        console.log('Something goes wrong!');
         console.log(error);
+        
     }
+
+
 }
